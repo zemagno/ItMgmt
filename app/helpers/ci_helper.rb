0166e5a1e1750b7d7c09617ef4d246fae04b4c6c
Jose Carlos Magno <zemagno@gmail.com>
2015-12-17 08:08:15 -0200

    Combobox do dicdados pegar dados de codigo ruby. ex: [Ci.where(:tipoci_id => 39).map(&:chave).uniq]

--- app/helpers/ci_helper.rb
+++ app/helpers/ci_helper.rb
@@ -2,14 +2,17 @@ module CiHelper
   def helper_gen_combobox(s, default)
 		@st = ""
 	    if s != nil then 
+	      if s =~ /\[(.*)\]/
+	         s = eval($1).join(",")
+	      end	
 	      s.split(',').each do |c|
 	      	if default==c
 	           @st = @st + "<option value='"+c+"' selected='selected'>"+c+"</option>"
 	        else
 	           @st = @st + "<option value='"+c+"'>"+c+"</option>"
 			end	      
-	      end
-	    end
+          end
+        end
 	    @st
 	 end
 
