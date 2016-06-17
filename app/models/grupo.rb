056ca6b17879f1af7dbc3fda5691081427238be7
Jose Carlos Magno <zemagno@gmail.com>
2016-06-17 08:16:13 -0300

    forcando salvar no database ActiveRecord::Base.partial_updates

--- app/models/grupo.rb
+++ app/models/grupo.rb
@@ -1,9 +1,9 @@
 class Grupo < ActiveRecord::Base
   attr_accessible :chamado, :dataCriacao, :dataValidade, :descricao, :membros, :nome, :solicitante
 
-  def to_s
-    "#{id}: #{nome} - #{membros}"
-  end
+  # def to_s
+  #   "#{id}: #{nome} - #{membros}"
+  # end
   # set_primary_key :nome
 
   # def self.getMembrosGrupo(grupo)
@@ -20,6 +20,7 @@ class Grupo < ActiveRecord::Base
   # end
 
   def self.addMembro(nomeGrupo,membro)
+    ActiveRecord::Base.partial_updates = false
     # se grupo nao existir, cria
 
     # _grupo = Grupo.find_or_create_by_nome(grupo)
@@ -28,15 +29,19 @@ class Grupo < ActiveRecord::Base
     _membros << ",#{membro}"
     puts "__Grupo.addMembro: [#{nomeGrupo}] [#{membro}] ___________ "
     _membros = _membros.split(",").compact.uniq.delete_if { |c| c == ""}.sort.join(",")
+    puts "____ Membros: #{_membros}"
     _grupo.membros = _membros
     _grupo.save!
+    ActiveRecord::Base.partial_updates = true
   end
 
   def self.delMembro(nomeGrupo,membro)
+    ActiveRecord::Base.partial_updates = false
     _grupo = Grupo.find_or_create_by_nome(nomeGrupo)
     _membros = _grupo.membros || ""
     _membros = _membros.split(",").compact.uniq.delete_if { |c| c == "" || c == membro}.sort.join(",")
     _grupo.membros = _membros
     _grupo.save!
+    ActiveRecord::Base.partial_updates = true
   end
 end
