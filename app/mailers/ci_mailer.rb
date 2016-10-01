da0e35e2cecd805deef0d064a299c2376f193bd2
Jose Carlos Magno <zemagno@gmail.com>
2016-10-01 12:48:33 -0300

    zendesk

--- app/mailers/ci_mailer.rb
+++ app/mailers/ci_mailer.rb
@@ -10,8 +10,12 @@ class CiMailer < ActionMailer::Base
   def enviar_anexo(template,_body,_subject,_to,_cc,_from,_attachment)
     @obj = _body
     attachments[_attachment.split("/").last] = File.read(_attachment)
-    mail(to: _to, subject: _subject, template_name: template, cc: _cc, from: _from)
 
+    puts "*******************************************************************************"
+    puts "to:#{_to} from:#{_from} cc:#{_cc} subject:#{_subject}"
+    puts "*******************************************************************************"
+    # mail(to: _to, subject: _subject, template_name: template, cc: _cc, from: _from)
+ 
   end
 
 
