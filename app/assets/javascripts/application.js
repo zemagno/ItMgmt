d2016a87d24657c169b88b163ef08a5addabe03f
Jose Carlos Magno <zemagno@gmail.com>
2016-11-27 07:16:51 -0200

    datatable fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(function() {
-        $( "#datepicker" ).datepicker({ dateFormat: "yy-mm-dd" });  
-});
+//$(function() {
+//        $( "#datepicker" ).datepicker({ dateFormat: "yy-mm-dd" });  
+//});
