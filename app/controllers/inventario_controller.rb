f09f74e5865f7765ed0e3a781843f6d0c2b50b25
Jose Carlos Magno <zemagno@gmail.com>
2016-12-20 07:39:58 -0200

    resolvendo merge conflit

--- app/controllers/inventario_controller.rb
+++ app/controllers/inventario_controller.rb
@@ -54,10 +54,6 @@ class InventarioController < ApplicationController
       @ci.descricao = _desc
       @ci._hostname = params[:hostname]
       @ci._inventariado = "INVENTARIADO"
-<<<<<<< HEAD
- 
-=======
->>>>>>> V6
       @ci.save!
       flash[:Info] = "Atualizado !"
     end
