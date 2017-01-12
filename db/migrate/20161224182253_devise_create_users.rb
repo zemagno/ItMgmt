ace0661289cea09e7c0414246266139e42fc346e
Jose Carlos Magno <zemagno@gmail.com>
2017-01-12 07:25:29 -0200

    drop table users antes do devise

--- db/migrate/20161224182253_devise_create_users.rb
+++ db/migrate/20161224182253_devise_create_users.rb
@@ -1,5 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
+    execute %Q{ DROP table users if exists users ; }
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
