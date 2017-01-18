d4c7800b160483efdc2e282a84b18348ef04ad5a
Jose Carlos Magno <zemagno@gmail.com>
2017-01-18 20:09:40 -0200

    audit sem sphinx e com pesquisa direta, com todos detalhes numa tela

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -2,23 +2,34 @@ class AuditsController < ApplicationController
   # GET /audits
   # GET /audits.json
   authorize_resource 
-  def index
-    @search = params[:search] || session[:search_audit]
-    session[:search_audit] = @search
+  # def index2
+  #   @search = params[:search] || session[:search_audit]
+  #   session[:search_audit] = @search
   
     
-    begin
-      Rails.logger.debug "[DEBUG] AuditsController:index search:[#{@search}] params:[#{params}]"
-      @audits = Audit.search @search, :match_mode => :boolean, :per_page => 20, :page => params[:page] #, :order => 'created_at DESC'
-      @audits.length
-      @audits.compact!
-    rescue => error
-      error.backtrace
-      Rails.logger.debug "[DEBUG] Error[DB0001]"
-      flash[:error] = "Error[DB0001] - Search Engine com Problema"
-      @audits = Audit.paginate(:page => params[:page])
-    end 
+  #   begin
+  #     Rails.logger.debug "[DEBUG] AuditsController:index search:[#{@search}] params:[#{params}]"
+  #     @audits = Audit.search @search, :match_mode => :boolean, :per_page => 20, :page => params[:page] #, :order => 'created_at DESC'
+  #     @audits.length
+  #     @audits.compact!
+  #   rescue => error
+  #     error.backtrace
+  #     Rails.logger.debug "[DEBUG] Error[DB0001]"
+  #     flash[:error] = "Error[DB0001] - Search Engine com Problema"
+  #     @audits = Audit.paginate(:page => params[:page])
+  #   end 
 
+  #   respond_to do |format|
+  #     format.html # index.html.erb
+  #     format.json { render json: @audits }
+  #   end
+  # end
+
+  def index
+    id = params[:id]
+    auditable_type = params[:auditable_type]
+    auditable_type_filho = params[:auditable_type_filho]
+    @audits = Audit.where("(auditable_id = ? and auditable_type = ?) or ( auditable_type = ? and associated_id = ?)",id,auditable_type,auditable_type_filho,id).order("created_at asc")
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @audits }
--- app/indices/audit_index.rb
+++ app/indices/audit_index.rb
@@ -1,8 +1,8 @@
-ThinkingSphinx::Index.define :audit, :with => :real_time do
-      indexes action , :as => :oque, :type => :string
-      indexes user_name, :as => :quem, :type => :string
-      indexes auditable_id, :as => :onde, :type => :integer
-      indexes auditable_type, :as => :tipoci, :type => :integer
-      indexes associated_id, :as => :relacionado, :type => :integer
-      indexes audited_changes, :as => :detalhe, :type => :integer
-  end
+# ThinkingSphinx::Index.define :audit, :with => :real_time do
+#       indexes action , :as => :oque, :type => :string
+#       indexes user_name, :as => :quem, :type => :string
+#       indexes auditable_id, :as => :onde, :type => :integer
+#       indexes auditable_type, :as => :tipoci, :type => :integer
+#       indexes associated_id, :as => :relacionado, :type => :integer
+#       indexes audited_changes, :as => :detalhe, :type => :integer
+#   end
